7da13c9436464680e6e5e116738c417e561e5a6c
vinopandi <126496644+vinopandi@users.noreply.github.com>
2023-03-02 13:01:10 +0530

    Update variables.tf
    removed

--- variables.tf
+++ variables.tf
@@ -3,9 +3,3 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
-variable “aws_access_key” {
-	default = “AKIAU64RZSDAVKYV5LEH”
-}
-variable "aws_secret_key" {
-	default = "iSVAGbGaWgseFmZK9a3dhcqQlfeNhLbPZB8mwQ4q"
-}
